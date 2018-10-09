0a4fb5a48d84f142ab743217e63e5ab9dea29e53
unknown <raduran8080@gmail.com>
2018-10-08 21:48:32 -0500

    tipos de datos y comentarios en js

---
+++ src/js/app.js
@@ -0,0 +1,29 @@
+// comentarios en js
+
+// una sola linea 
+
+/*
+
+multiples lineas
+
+*/
+
+
+/**Multiples
+ * lineas
+ * con formato
+ */
+
+//NUmero
+var numero = 2453.563;
+//string
+var string = " esto es un string";
+var string2 = 'esto es una string'
+var string3 = `esto es un string`
+    //boolean
+var boolean1 = true;
+var boolean2 = false;
+//Null
+var vacio = null;
+//undefined
+var indefinido = undefined;
